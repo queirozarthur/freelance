872deaba8d23798ac4f42ce4e690225492c4c0b5
queirozarthur <queirozarthur2109@gmail.com>
2025-12-08 14:32:28 -0300

    ajuste no pagamento e no admin

--- loja/forms.py
+++ loja/forms.py
@@ -1,10 +1,17 @@
 from django import forms
-from .models import Produto, Colecao
+from .models import Produto, Colecao, Tamanho
 
 class ProdutoForm(forms.ModelForm):
+    tamanhos = forms.ModelMultipleChoiceField(
+        queryset=Tamanho.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+        label="Tamanhos Disponíveis"
+    )
+
     class Meta:
         model = Produto
-        fields = ['colecao','nome', 'preco', 'descricao', 'foto_principal', 'ativo',] 
+        fields = ['colecao','nome', 'preco', 'descricao', 'foto_principal', 'ativo','tamanhos',] 
         
 
     def __init__(self, *args, **kwargs):
@@ -12,6 +19,7 @@ class ProdutoForm(forms.ModelForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'w-full px-4 py-3 rounded-lg bg-gray-50 border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-gray-900 transition duration-200 outline-none'
             field.widget.attrs['placeholder'] = f'Digite {field.label.lower()}...'
+        self.fields['tamanhos'].widget.attrs['class'] = 'flex flex-wrap gap-4 mt-2'
 
 class ColecaoForm(forms.ModelForm):
     class Meta:
